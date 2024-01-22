f894a596589c8ee3ca640f252f39dbc6415c3f67
dhruv sakariya <dhruvsakariya2304@gmail.com>
2024-01-23 00:06:05 +0530

    🐛 Removed python solution 🐛 Added Code Space in two sum problem

--- src/utils/problems/two-sum.ts
+++ src/utils/problems/two-sum.ts
@@ -137,6 +137,7 @@ import java.util.Scanner;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+
     }
 
     public static void main(String[] args) {
--- src/utils/problems/valid-parentheses.ts
+++ src/utils/problems/valid-parentheses.ts
@@ -61,16 +61,7 @@ export const validParentheses: Problem = {
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        d = {'(': ')', '{': '}', '[': ']'}
-        stack = []
 
-        for i in s:
-            if i in d:
-                stack.append(i)
-            elif len(stack) == 0 or d[stack.pop()] != i:
-                return False
-
-        return len(stack) == 0
 
 def main():
     t = int(input())
